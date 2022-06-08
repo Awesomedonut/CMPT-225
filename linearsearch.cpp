96aea3a1e454a9c597f7f5875feae163843561ea
Awesomedonut <jasperyjsong@gmail.com>
2022-06-07 19:02:53 -0700

    recursive implementation

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -9,12 +9,22 @@ int iterative(int size, int target, int* arr){
     return -1;
 }
 
-int recursive(int size, int target, int* arr){
+int recursive(int target, int* arr, int i){
+    if(arr[i] == target){
+        return i;
+    }else{
+        return recursive(target, arr, i+1);
+    }
+    //if(found)
+
+    //else recursive(int size-1, int target, int* arr)
     return 0;
 }
 
 int main(){
     int arr[6] = {1, 5, 6, 2, 0, 5};
-    iterative(6, 0, arr);
+    std::cout << iterative(6, 0, arr) << "\n";
+    std::cout << recursive(0, arr, 0);
+
     return 0;
 }
